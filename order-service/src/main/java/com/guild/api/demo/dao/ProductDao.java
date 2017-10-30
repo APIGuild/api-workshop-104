969fd397c039109d220e8dda6667415667d5576f
Waterstrong <sqlin@thoughtworks.com>
2017-10-31 00:23:44 +0800

    add rest utils for hystrix and http connection properties

--- order-service/src/main/java/com/guild/api/demo/dao/ProductDao.java
+++ order-service/src/main/java/com/guild/api/demo/dao/ProductDao.java
@@ -1,29 +1,31 @@
 package com.guild.api.demo.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import com.guild.api.demo.dao.exception.DaoException;
+import com.guild.api.demo.dao.exception.DaoExceptionBuilder;
 import com.guild.api.demo.model.ProductModel;
+import com.guild.api.demo.util.rest.PooledRestTemplateBuilder;
+import com.guild.api.demo.util.rest.RestEndpointProperties;
 
 @Component
 public class ProductDao {
     private static final String RETRIEVE_PRODUCT_URL = "{baseUrl}/products/{productId}";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private RestEndpointProperties properties = new RestEndpointProperties();
 
-    @Value("${product.service.baseUrl}")
-    private String baseUrl;
-
-    public ProductModel getProduct(String productId) {
-        String url = UriComponentsBuilder
-                .fromPath(RETRIEVE_PRODUCT_URL)
-                .buildAndExpand(baseUrl, productId)
-                .toString();
-        String productInfo = restTemplate.getForEntity(url, String.class).getBody();
-        return new ProductModel(productId, productInfo);
+    public ProductModel getProduct(String productId) throws DaoException {
+        String url = UriComponentsBuilder.fromPath(RETRIEVE_PRODUCT_URL)
+                .buildAndExpand(properties.getBaserUrl(), productId).toString();
+        try {
+            String productInfo = new PooledRestTemplateBuilder(properties)
+                    .build()
+                    .execute("ProductService", restTemplate -> restTemplate.getForEntity(url, String.class))
+                    .getBody();
+            return new ProductModel(productId, productInfo);
+        } catch (Exception exception) {
+            throw new DaoExceptionBuilder(url).build(exception);
+        }
     }
 
 }
